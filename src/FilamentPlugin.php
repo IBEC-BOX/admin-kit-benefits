f1d7b7b5c3285515a48d0c06d3abcfb7de334ccb
Anastas Mironov <37507557+ast21@users.noreply.github.com>
2024-07-19 18:25:30 +0900

    Initial commit

---
+++ src/FilamentPlugin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace VendorName\Skeleton;
+
+use Filament\Contracts\Plugin;
+use Filament\Panel;
+use VendorName\Skeleton\UI\Filament\Resources\SingleNameResource;
+
+class FilamentPlugin implements Plugin
+{
+    public function getId(): string
+    {
+        return 'filament-plugin-:package_slug';
+    }
+
+    public function register(Panel $panel): void
+    {
+        $panel->resources([
+            SingleNameResource::class,
+        ]);
+    }
+
+    public function boot(Panel $panel): void {}
+
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+}
